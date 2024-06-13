f6e2f19211b832f2d0a8cfb727606110202cc9a6
Luan Castro <luanhcvalese@gmail.com>
2024-06-13 19:10:13 -0300

    updating prisma migration

--- prisma/migrations/20240613213956_/migration.sql
+++ prisma/migrations/20240613213956_/migration.sql
@@ -4,9 +4,10 @@ CREATE TABLE "Survey" (
     "nicheId" TEXT NOT NULL,
     "stars" INTEGER NOT NULL,
     "email" TEXT NOT NULL,
-    "nicheAnswers" JSONB NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "nicheAnswers" JSONB,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) NOT NULL,
+    "deleted" TIMESTAMP(3),
 
     CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
 );
@@ -16,7 +17,6 @@ CREATE TABLE "Niche" (
     "id" TEXT NOT NULL,
     "key" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deleted" TIMESTAMP(3),
 
@@ -27,7 +27,6 @@ CREATE TABLE "Niche" (
 CREATE TABLE "NicheQuestion" (
     "id" TEXT NOT NULL,
     "key" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
     "label" TEXT NOT NULL,
     "niches" TEXT[],
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
