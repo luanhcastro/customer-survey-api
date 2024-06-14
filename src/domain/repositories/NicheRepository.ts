5e5477b7b1b7f8b99cf0c0229ba14c822442d8f2
Luan Castro <luanhcvalese@gmail.com>
2024-06-14 13:45:10 -0300

    niche repository

--- src/domain/repositories/NicheRepository.ts
+++ src/domain/repositories/NicheRepository.ts
@@ -2,6 +2,7 @@ import { Niche } from '../entities/Niche';
 
 export interface NicheRepository {
   findById(id: string): Promise<Niche | null>;
+  findAll(): Promise<Niche[] | null>;
   create(niche: Niche): Promise<void>;
   update(niche: Niche): Promise<void>;
 }
