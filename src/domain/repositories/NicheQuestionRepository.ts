9f2f6622d4c93e00e1b82f476ff59bdd6c5e0dca
Luan Castro <luanhcvalese@gmail.com>
2024-06-14 13:44:46 -0300

    niche question repository

--- src/domain/repositories/NicheQuestionRepository.ts
+++ src/domain/repositories/NicheQuestionRepository.ts
@@ -2,6 +2,7 @@ import { NicheQuestion } from '../entities/NicheQuestion';
 
 export interface NicheQuestionRepository {
   findById(id: string): Promise<NicheQuestion | null>;
+  findManyByNiche(nicheId: string): Promise<NicheQuestion[] | null>
   create(nicheQuestion: NicheQuestion): Promise<void>;
   update(nicheQuestion: NicheQuestion): Promise<void>;
 }
